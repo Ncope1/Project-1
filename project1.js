d5678aeb7f3f75b4d2bcd8da6914b5efd5ec393b
Natalee C <COPENATALEE@ME.COM>
2017-12-11 23:48:14 -0500

    updated css to center all & addes js for DOM manipulation

--- project1.js
+++ project1.js
@@ -1,1 +1,21 @@
 console.log("wazzup!")
+
+// create a score counter
+var buttonsCorrect = document.getElementsByClassName('correct')
+
+
+for(var i = 0; i < buttonsCorrect.length; i ++){
+    buttonsCorrect[i].addEventListener('click', console.log('Irreplaceable'))
+}
+
+
+//make one question & matching set of answer options appear on screen at a time
+
+//screen says "Make Lemonade" when game ends
+
+//make answer buttons line up on the page in 2 rows and two columns with questions above
+
+//make the h1 remain static on the page as each question iterates
+
+
+
