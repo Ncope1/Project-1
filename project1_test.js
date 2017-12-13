d1ebab9cc31c37c3ae14145bbbe2a5b71b978202
Natalee C <COPENATALEE@ME.COM>
2017-12-13 12:11:18 -0500

    started adding info to README.md file

--- project1_test.js
+++ project1_test.js
@@ -39,7 +39,6 @@ function showScores () {
     element.innerHTML = gameOverHtml
 }
 
-
 var questions = [
     new Question("What is Beyoncé's middle name?", ["Erica", "Sasha", "Giselle", "Gabrielle"], "Giselle"),
     new Question("What is Beyoncé's favorite color?", ["Blue", "Green", "Yellow", "Red"], "Blue"),
