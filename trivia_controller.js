a2b3d54850475c63c8c9225d2ffc0346458f658f
Natalee C <COPENATALEE@ME.COM>
2018-01-16 15:12:40 -0500

    added new branch for resubmit

--- trivia_controller.js
+++ trivia_controller.js
@@ -28,24 +28,3 @@ class Trivia {
     
 }
 }
-
-
-// Trivia.prototype.getQuestionIndex = function () //gets index of current question
-// {
-//     return this.questions[this.questionIndex]
-// }
-
-// Trivia.prototype.hasEnded = function () {
-//     return this.questions.length === this.questionIndex
-// }
-
-// Trivia.prototype.guess = function (answer) //checks if the current answer is equal to the answer selected by the user
-// {
-//     if (this.getQuestionIndex().correctAnswer(answer)) {
-//         this.score++ //increase score within the condition
-
-//     }
-//     this.questionIndex++
-// }
-
-
