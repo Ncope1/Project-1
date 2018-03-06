794e4d8371d896e0b01457539dc64c9921093aa4
Natalee C <COPENATALEE@ME.COM>
2018-03-06 14:48:48 -0500

    added additional js

--- README.md
+++ README.md
@@ -2,21 +2,21 @@
 The objective of this project was to create a trivia game. I created a six (6) question trivia based on Pop Artist, Beyoncé and her personal/professional life.
 
 ## Brief example of rendered Trivia
-Please click this [link](https://raw.githubusercontent.com/Ncope1/Project-1/master/Screen%20Shot%202017-12-13%20at%209.35.02%20PM.png) for a screen shot of the rendered trivia image
+![alt text](https://raw.githubusercontent.com/Ncope1/Project-1/master/Screen%20Shot%202018-03-06%20at%202.19.26%20PM.png "project1 screenshot")
 
 ## List of Features
-* One question and a set of answer options are displayed one at a time when the page renders
+* One question and a set of answer options are displayed when the page renders
 
-* Trivia progresses to the next page when an answer option is selected
+* Trivia progresses to the next page when an answer option is selected and a one point score is shown if the question is answered correctly
 
-* Once all questions have been answered, “Game Over” is displayed on the final page along with the user’s score
+* Once all questions have been answered, “Game Over” is displayed on the final page along with the user’s final score
 
 ## List of Technologies Used
 * HTML
 
 * CSS
 
-* Vanilla JavaScript
+* JavaScript
 
 ## Installation Instructions
 Users are able to access this trivia via their web browser by clicking this [link](https://ncope1.github.io/Project-1/)
@@ -33,13 +33,10 @@ I received guidance from online resources as well as my peers. Please see below 
 * [Stack Overflow](https://stackoverflow.com/questions/18546038/store-javascript-variable-into-array)
 * [Free Code Camp](https://www.freecodecamp.org/challenges/store-multiple-values-in-one-variable-using-javascript-arrays)
 
-Thanks to all of my WDI-20 instructors and peers for your guidance in helping me to figure out my errors.
-
 ## Future Improvements
 In order to improve this project, my plan is to:
 * Rewrite my code so that it is much more DRY
 * Include images/GIFs of Beyoncé
-
 
 ## Contribution Guidelines
 I welcome any advice that will enhance my app and make my coding cleaner. Feel free to contribute via this [link](https://github.com/Ncope1/Project-1).
--- trivia_controller.js
+++ trivia_controller.js
@@ -1,74 +1,51 @@
-let changeQuestion = document.getElementsByClassName('option')
+//this is the game functionality - keeping track of the score and the number of questions the trivia contains. also contains the current question and if the game has ended 
+
+//this is the game functionality - keeping track of the score and the number of questions the trivia contains. also contains the current question and if the game has ended 
+class Trivia {
+    constructor(questions) {//constructor function
+    this.score = 0
+    this.questions = questions
+    this.questionIndex = 0
+    this.getQuestionIndex = function () //gets index of current question
+        {
+            return this.questions[this.questionIndex]
+        }
+    this.guess = function () {
+        return this.questions.length === this.questionIndex
+    }
+    this.guess = function (answer) //checks if the current answer is equal to the answer selected by the user
+    {
+        if (this.getQuestionIndex().correctAnswer(answer)) {
+            this.score++ //increase score within the condition
+            var scoreSpan = document.getElementsByClassName('score')[0]
+            scoreSpan.innerHTML = this.score
+        }
+        this.questionIndex++
+    }
+    this.hasEnded = function () {
+        return this.questions.length === this.questionIndex
+    }
     
-function firstQuestion () {
-    // document.getElementById('Question1').innerHTML = allQuestions[0].question
-    var correctAnswer = document.getElementById('option-c')
-    correctAnswer.addEventListener('click', function() {
-        console.log('working')
-        game.score++
-        console.log(game.score)
-        userScore()
-    })
 }
-firstQuestion()
-
-
-function secondQuestion () {
-    correctAnswer = document.getElementById('option-e')
-    correctAnswer.addEventListener('click', function() {
-      game.score++
-      userScore()
-    })
 }
-secondQuestion()
 
 
-function thirdQuestion () {
-    correctAnswer = document.getElementById('option-k')
-    correctAnswer.addEventListener('click', function() {
-      game.score++
-      userScore()
-    })
-}
-thirdQuestion()
+// Trivia.prototype.getQuestionIndex = function () //gets index of current question
+// {
+//     return this.questions[this.questionIndex]
+// }
 
+// Trivia.prototype.hasEnded = function () {
+//     return this.questions.length === this.questionIndex
+// }
 
-function fourthQuestion () {
-    correctAnswer = document.getElementById('option-m')
-    correctAnswer.addEventListener('click', function() {
-      game.score++
-      userScore()
-    })
-    }
-fourthQuestion()
+// Trivia.prototype.guess = function (answer) //checks if the current answer is equal to the answer selected by the user
+// {
+//     if (this.getQuestionIndex().correctAnswer(answer)) {
+//         this.score++ //increase score within the condition
 
+//     }
+//     this.questionIndex++
+// }
 
-
-function fifthQuestion () {
-    correctAnswer = document.getElementById('option-s')
-    correctAnswer.addEventListener('click', function() {
-      game.score++
-      userScore()
-    })
-    }
-fifthQuestion()
-
-
-function sixthQuestion () {
-    correctAnswer = document.getElementById('option-x')
-    correctAnswer.addEventListener('click', function() {
-      game.score++
-      userScore()
-    })
-    }
-sixthQuestion()
-
-
-var game = {
-    score: 0
-}
-        
-function userScore () {
-     document.getElementsByClassName('score')[0].innerHTML = `Score: ${game.score}`
-}
 
